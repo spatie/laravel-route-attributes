bc7bdd40e308a7002887f1d07da9b050307bf847
freekmurze <freekmurze@users.noreply.github.com>
2020-10-28 07:46:34 +0000

    Fix styling

--- tests/TestClasses/Controllers/RouteAttribute/RouteNameTestController.php
+++ tests/TestClasses/Controllers/RouteAttribute/RouteNameTestController.php
@@ -4,8 +4,7 @@ namespace Spatie\RouteAttributes\Tests\TestClasses\Controllers\RouteAttribute;
 
 use Spatie\RouteAttributes\Attributes\Route;
 
-class
-RouteNameTestController
+class RouteNameTestController
 {
     #[Route('get', 'my-method', name: 'test-name')]
     public function myMethod()
