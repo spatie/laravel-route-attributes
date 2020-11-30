a2c2246028260cbbc343eb6d60763bb2e9ffaf36
Robert Dennis <robertdennispersonal@gmail.com>
2020-11-30 04:47:11 -0500

    Create DomainAttributeTest.php

---
+++ tests/AttributeTests/DomainAttributeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Spatie\RouteAttributes\Tests\AttributeTests;
+
+use Spatie\RouteAttributes\Tests\TestCase;
+use Spatie\RouteAttributes\Tests\TestClasses\Controllers\DomainTestController;
+
+class DomainAttributeTest extends TestCase
+{
+    /** @test */
+    public function it_can_apply_a_domain_on_the_url_of_every_method()
+    {
+        $this->routeRegistrar->registerClass(DomainTestController::class);
+
+        $this
+            ->assertRegisteredRoutesCount(2)
+            ->assertRouteRegistered(
+                DomainTestController::class,
+                controllerMethod: 'myGetMethod',
+                uri: 'my-get-method',
+                domain: 'my-subdomain.localhost'
+            )
+            ->assertRouteRegistered(
+                DomainTestController::class,
+                controllerMethod: 'myPostMethod',
+                httpMethod: 'post',
+                uri: 'my-post-method',
+                domain: 'my-subdomain.localhost'
+            );
+    }
+}
