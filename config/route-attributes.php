06ebea1504cb5060c0df0abab24b3133c954d873
Patrick Heppler <12952240+HepplerDotNet@users.noreply.github.com>
2020-12-24 17:29:53 +0100

    Update route-attributes.php
    IMHO using the Middleware Group 'web' defined in Kernel.php makes more sense, as this is how Laravel RouteServiceProvider registers web routes.

--- config/route-attributes.php
+++ config/route-attributes.php
@@ -18,6 +18,6 @@ return [
      * This middleware will be applied to all routes.
      */
     'middleware' => [
-        \Illuminate\Routing\Middleware\SubstituteBindings::class
+       'web',
     ]
 ];
