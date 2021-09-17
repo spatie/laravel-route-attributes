76029952c7272feb6f7688cb1a897cf514ddc23c
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-09-17 09:46:36 +0200

    feat: add Group attribute

---
+++ src/Attributes/Group.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Spatie\RouteAttributes\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+class Group implements RouteAttribute
+{
+    public function __construct(
+        public ?string $prefix = null,
+        public ?string $domain = null,
+        public ?string $as = null,
+        public ?array $where = [],
+    ) {
+    }
+}
