07e56ff0fdbcd6bb595ffb9431e7716985eebef0
Rico Leuthold <rico.leuthold@smartcode.ch>
2023-11-23 12:09:26 +0100

    Make Get Attribute repeatable and therefore allow multiple get routes to be added to a method.

---
+++ tests/TestClasses/Controllers/GetMultipleTestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Spatie\RouteAttributes\Tests\TestClasses\Controllers;
+
+use Spatie\RouteAttributes\Attributes\Get;
+
+class GetMultipleTestController
+{
+    #[Get('my-get-method')]
+    #[Get('my-other-get-method')]
+    public function myGetMethod()
+    {
+    }
+}
